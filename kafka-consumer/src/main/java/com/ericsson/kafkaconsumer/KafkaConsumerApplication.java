44dd1aacf70f7fae77d5bdeddcc6ccf5a41855cf
danjiehu <contact.dhu@gmail.com>
2024-05-24 14:50:50 +0100

    added kafka consumer and procedure

---
+++ kafka-consumer/src/main/java/com/ericsson/kafkaconsumer/KafkaConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.ericsson.kafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerApplication.class, args);
+        System.out.println("KafkaConsumerApplication running...");
+    }
+
+}
