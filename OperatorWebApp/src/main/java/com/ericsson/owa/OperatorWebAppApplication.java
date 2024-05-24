16c72764025cfffd51a34b46c7c94461da321b4b
Tadhg <A00315057@student.tus.ie>
2024-05-24 10:17:52 +0100

    Initial Commit, Added Operator-Web-App

---
+++ OperatorWebApp/src/main/java/com/ericsson/owa/OperatorWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ericsson.owa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperatorWebAppApplication.class, args);
+    }
+
+}
---
+++ OperatorWebApp/src/test/java/com/ericsson/owa/OperatorWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsson.owa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatorWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
