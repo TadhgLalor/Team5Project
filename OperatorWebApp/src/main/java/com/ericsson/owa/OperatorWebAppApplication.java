76b26f29577a26bbd46f418229ee5291c907f63d
danjiehu <contact.dhu@gmail.com>
2024-05-25 14:24:06 +0100

    updat jekins file

--- OperatorWebApp/src/main/java/com/ericsson/owa/OperatorWebAppApplication.java
+++ OperatorWebApp/src/main/java/com/ericsson/owa/OperatorWebAppApplication.java
@@ -8,6 +8,7 @@ public class OperatorWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OperatorWebAppApplication.class, args);
+        System.out.println("OperatorWebAppApplication running...");
     }
 
 }
