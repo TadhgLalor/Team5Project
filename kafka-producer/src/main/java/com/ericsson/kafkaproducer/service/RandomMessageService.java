10b99e8fcaabf7df3c1768aa5ff132a9a0ee40df
danjiehu <contact.dhu@gmail.com>
2024-05-29 16:58:16 +0100

    fix security hotspot - change to secure random

--- kafka-producer/src/main/java/com/ericsson/kafkaproducer/service/RandomMessageService.java
+++ kafka-producer/src/main/java/com/ericsson/kafkaproducer/service/RandomMessageService.java
@@ -4,20 +4,21 @@ import com.ericsson.kafkaproducer.dto.CallFault;
 import com.ericsson.kafkaproducer.dto.FaultReason;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.util.Random;
 import java.util.UUID;
 
 @Service
 public class RandomMessageService {
 
-    private final Random random = new Random();
+    private final SecureRandom secureRandom = new SecureRandom();
 
     public CallFault generateRandomFaultMessage(){
 
-        int nodeId = random.nextInt(5);      //Between 0-999
-        int networkId = random.nextInt(1000);
+        int nodeId = secureRandom.nextInt(5);      //Between 0-999
+        int networkId = secureRandom.nextInt(1000);
         String networkName = randomNetworkName(networkId);
-        int callerId = random.nextInt(1000);
+        int callerId = secureRandom.nextInt(1000);
         String callerName = randomomString();
         // Random FaultReason enum value
         FaultReason faultReason = randomFaultReason();
