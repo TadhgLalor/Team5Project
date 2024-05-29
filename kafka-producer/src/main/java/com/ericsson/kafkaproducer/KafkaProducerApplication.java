86a775a9a1f365dcbe6aacd763ae0cf4b01a8668
danjiehu <contact.dhu@gmail.com>
2024-05-29 23:28:50 +0100

    refactor for code smell

--- kafka-producer/src/main/java/com/ericsson/kafkaproducer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/com/ericsson/kafkaproducer/KafkaProducerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.logging.Logger;
-import java.util.logging.Level;
 
 @SpringBootApplication
 public class KafkaProducerApplication implements ApplicationRunner {
