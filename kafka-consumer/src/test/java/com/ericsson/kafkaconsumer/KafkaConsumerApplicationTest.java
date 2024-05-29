7e62c1b3f01dc4f47d0172e33bcad635c6b1510c
danjiehu <contact.dhu@gmail.com>
2024-05-29 22:01:03 +0100

    optimize jenkins

--- kafka-consumer/src/test/java/com/ericsson/kafkaconsumer/KafkaConsumerApplicationTest.java
+++ kafka-consumer/src/test/java/com/ericsson/kafkaconsumer/KafkaConsumerApplicationTest.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
+@SpringBootTest
 class KafkaConsumerApplicationTests {
 
     @Test
